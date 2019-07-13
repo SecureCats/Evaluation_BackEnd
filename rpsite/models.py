197e7b2c9ed108e08c0981f31c0b6de9263a9513
felinae98 <731499577@qq.com>
2019-07-13 11:57:58 +0800

    create evaluation table

--- rpsite/models.py
+++ rpsite/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+
+    question_text = models.CharField('问题描述', max_length=100)
+
+    def __str__(self):
+        return self.question_text
+
+
+class Option(models.Model):
+
+    CHOICES = [
+        ('A', 'A'),
+        ('B', 'B'),
+        ('C', 'C'),
+        ('D', 'D')
+    ]
+    question = models.ForeignKey(Question, models.CASCADE)
+    option_text = models.CharField("选项描述", max_length=100)
+    option_choice = models.CharField('选项', max_length=1, choices=CHOICES)
+
+    def __str__(self):
+        return '{}-{}'.format(self.option_choice, self.option_text)
+
