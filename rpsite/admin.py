385de1b7b7b26047314425108a55c42ccb5b6ec5
felinae98 <731499577@qq.com>
2019-07-13 15:39:10 +0800

    hack the model and admin site

--- rpsite/admin.py
+++ rpsite/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+class OptionInline(admin.StackedInline):
+    model = models.Option
+    extra = 2
+    max_num = 4
+
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [OptionInline]
+
+class StudentParticipation(admin.StackedInline):
+    model = models.Course.teaching_class.through
+    extra = 1
+
+@admin.register(models.Course)
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [
+        StudentParticipation
+    ]
+    exclude = ('teaching_class',)
+    list_display = [
+        '__str__', 'course_no', 'semaster'
+    ]
+
+@admin.register(models.TeachingClass)
+class TeachingClass(admin.ModelAdmin):
+    inlines = [
+        StudentParticipation
+    ]
+
+class EvaluationItemInline(admin.StackedInline):
+    model = models.EvaluationItem
+    extra = 0
+    fields = ['question', 'option']
+    readonly_fields = ['question', 'option']
+
+@admin.register(models.Evaluation)
+class EvaluationAdmin(admin.ModelAdmin):
+    inlines = [EvaluationItemInline]
--- rpsite/models.py
+++ rpsite/models.py
@@ -3,6 +3,9 @@ from django.db import models
 # Create your models here.
 class Question(models.Model):
 
+    class Meta:
+        ordering = ['id']
+
     question_text = models.CharField('问题描述', max_length=100)
 
     def __str__(self):
@@ -24,3 +27,41 @@ class Option(models.Model):
     def __str__(self):
         return '{}-{}'.format(self.option_choice, self.option_text)
 
+class TeachingClass(models.Model):
+    # this class is just for many to many model relationship
+
+    classno = models.CharField('班号', max_length=20)
+
+    def __str__(self):
+        return self.classno
+
+
+class Course(models.Model):
+
+    course_name = models.CharField('课程名称', max_length=20)
+    course_no = models.CharField('课程编号', unique=True, max_length=20)
+    semaster = models.CharField('学期', max_length=20)
+    teaching_class = models.ManyToManyField(TeachingClass)
+
+    def __str__(self):
+        return self.course_name
+
+
+class Evaluation(models.Model):
+
+    rnym = models.CharField("rnym", max_length=100)
+    course = models.ForeignKey(Course, models.CASCADE)
+
+    def __str__(self):
+        return '{}-{}'.format(self.course.course_name, hex(int(self.rnym))[2:10])
+
+class EvaluationItem(models.Model):
+
+    option = models.ForeignKey(Option, models.CASCADE)
+    evaluation = models.ForeignKey(Evaluation, models.CASCADE)
+
+    def question(self):
+        return self.option.question.question_text
+
+    def __str__(self):
+        return '{}-{}'.format(str(self.evaluation), self.option.question.id)
