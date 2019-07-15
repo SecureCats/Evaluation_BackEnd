0df1722a729d53809ab74d24c3acf44e83244c2b
felinae98 <731499577@qq.com>
2019-07-15 14:47:38 +0800

    verify pass

--- rpsite/tests.py
+++ rpsite/tests.py
@@ -1,3 +1,82 @@
 from django.test import TestCase
+from django.conf import settings
+from .utils import verify, test_privkey, test_pubkey, hashing
+from Crypto.Util import number
+import random
+import gmpy2
+from hashlib import sha256
 
 # Create your tests here.
+class VeryfyTest(TestCase):
+
+    def test_verify_function(self):
+        pubkey = test_pubkey
+        uk = 123333
+        q = pubkey['n'] // test_privkey
+        s = random.randrange(1<<8196)
+        e = number.getPrime(2050)
+        d = int(gmpy2.invert(e, (test_privkey-1)*(q-1)))
+        tmp = pow(pubkey['a'], uk, pubkey['n']) * \
+            pow(pubkey['b'], s, pubkey['n']) * \
+            pubkey['c'] % pubkey['n']
+        v = pow(tmp, d, pubkey['n'])
+        left = pow(v, e, pubkey['n'])
+        right = tmp
+        self.assertEqual(left, right)
+        # the signiture is (e, v, s)
+        classno = 666
+        grnym = int(sha256(str(classno).encode()).hexdigest(), 16) % 731499577
+        grnym = pow(grnym, settings.RNYM_PARAM['exp'], settings.RNYM_PARAM['gamma'])
+        params = {}
+        priv = {}
+        # generate randoms
+        for i in ['s', 'e', 'w', 'z', 'x']:
+            priv['r'+i] = random.randrange(1<<32)
+        for i in range(1, 20):
+            priv['r{}'.format(i)] = random.randrange(1<<32)
+        priv['w'] = random.randrange(1<<32)
+        priv['r'] = random.randrange(1<<32)
+        priv['r_'] = priv['rz'] - e * priv['rw']
+        # calc Cs
+        params['Cs'] = pow(pubkey['g'], s, pubkey['n']) * pow(pubkey['h'], priv['rs'], pubkey['n']) % pubkey['n']
+        params['Ce'] = pow(pubkey['g'], e, pubkey['n']) * pow(pubkey['h'], priv['re'], pubkey['n']) % pubkey['n']
+        params['Cv'] = v * pow(pubkey['g'], priv['w'], pubkey['n']) % pubkey['n']
+        params['Cw'] = pow(pubkey['g'], priv['w'], pubkey['n']) * pow(pubkey['h'], priv['rw'], pubkey['n']) % pubkey['n']
+        priv['z'] = e * priv['w']
+        params['C'] = pow(params['Cv'], e, pubkey['n']) * pow(pubkey['h'], priv['r'], pubkey['n']) % pubkey['n']
+        params['Cx'] = pow(pubkey['g'], uk, pubkey['n']) * pow(pubkey['h'], priv['rx'], pubkey['n']) % pubkey['n']
+        params['Cz'] = pow(pubkey['g'], priv['z'], pubkey['n']) * pow(pubkey['h'], priv['rz'], pubkey['n']) % pubkey['n']
+        # calc ys
+        params['y1'] = pow(params['Cv'], priv['r1'], pubkey['n']) * pow(pubkey['h'], priv['r2'], pubkey['n']) % pubkey['n']
+        params['y2'] = pow(pubkey['g'], priv['r1'], pubkey['n']) * pow(pubkey['h'], priv['r3'], pubkey['n']) % pubkey['n']
+        params['y3'] = pow(pubkey['a'], priv['r4'], pubkey['n']) * pow(pubkey['b'], priv['r5'], pubkey['n']) * \
+            pow(pubkey['g'], priv['r6'], pubkey['n']) * pow(pubkey['h'], priv['r7'], pubkey['n']) % pubkey['n']
+        params['y4'] = pow(pubkey['g'], priv['r4'], pubkey['n']) * pow(pubkey['h'], priv['r8'], pubkey['n']) % pubkey['n']
+        params['y5'] = pow(pubkey['g'], priv['r5'], pubkey['n']) * pow(pubkey['h'], priv['r9'], pubkey['n']) % pubkey['n']
+        params['y6'] = pow(pubkey['g'], priv['r10'], pubkey['n']) * pow(pubkey['h'], priv['r11'], pubkey['n']) % pubkey['n']
+        params['y7'] = pow(pubkey['g'], priv['r6'], pubkey['n']) * pow(pubkey['h'], priv['r12'], pubkey['n']) % pubkey['n']
+        params['y8'] = pow(params['Cv'], priv['r10'], pubkey['n']) * pow(pubkey['h'], priv['r7'], pubkey['n']) % pubkey['n']
+        params['y9'] = pow(pubkey['g'], priv['r13'], pubkey['n']) * pow(pubkey['h'], priv['r14'], pubkey['n']) % pubkey['n']
+        params['y10'] = pow(pubkey['g'], priv['r15'], pubkey['n']) * pow(pubkey['h'], priv['r16'], pubkey['n']) % pubkey['n']
+        params['y11'] = pow(pubkey['g'], priv['r17'], pubkey['n']) * pow(pubkey['h'], priv['r18'], pubkey['n']) % pubkey['n']
+        params['y12'] = pow(params['Cw'], priv['r17'], pubkey['n']) * pow(pubkey['h'], priv['r19'], pubkey['n']) % pubkey['n']
+        params['y13'] = pow(grnym, priv['r4'], settings.RNYM_PARAM['gamma'])
+        # clac x
+        params['x'] = hashing(
+            params['C'], params['Cv'], params['Ce'],
+            params['Cs'], params['Cx'], params['Cz'],
+            params['Cw'], pubkey['g'], pubkey['h']
+        )
+        # clac z
+        for i, j in enumerate((
+            e, priv['r'], priv['re'], uk, s,
+            priv['z'], priv['r'], priv['rx'], priv['rs'], e,
+            priv['re'], priv['rz'], priv['z'], priv['rz'], priv['w'],
+            priv['rw'], e, priv['re'], priv['r_']
+            )):
+            params['z{}'.format(i+1)] = priv['r{}'.format(i+1)] + params['x'] * j
+        # clac rnym
+        params['rnym'] = pow(grnym, uk, settings.RNYM_PARAM['gamma'])
+        # vrfy
+        self.assertIsInstance(verify(pubkey, str(classno), **params), bool)
+        
--- rpsite/views.py
+++ rpsite/views.py
@@ -2,15 +2,21 @@ from django.shortcuts import render, get_object_or_404, \
     get_list_or_404
 from django.http import JsonResponse
 from . import models
+from .utils import test_pubkey
+from django.conf import settings
+import requests
+import json
 
 # Create your views here.
+
+
 def query_question_list(request):
     classno = request.GET['classno']
     semester = request.GET['semester']
     course_list = get_list_or_404(
-        models.Course, 
-        teaching_class__classno=classno, 
-        semester=semester    
+        models.Course,
+        teaching_class__classno=classno,
+        semester=semester
     )
     questions = []
     question_set = models.Question.objects.all()
@@ -30,10 +36,20 @@ def query_question_list(request):
     for course in course_list:
         tasks.append({
             'id': course.course_no,
-            'title': course.course_name, 
+            'title': course.course_name,
             'status': 0,
             'questions': questions
         })
     return JsonResponse({
         'tasks': tasks
     })
+
+
+def get_pubkey(classno, semester):
+    if settings.PUBKEY_TESTING:
+        return test_pubkey
+    aip_site = settings.AIP_URL if settings.AIP_URL.endswith('/')  \
+        else settings.AIP_URL + '/'
+    res = requests.get('{}api/v1/pubkey/{}/{}'.format(aip_site,semester, classno))
+    res_dic = json.loads(res.text)
+    return {key:int(res_dic[key]) for key in res_dic}
